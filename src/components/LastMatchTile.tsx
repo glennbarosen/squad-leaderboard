ac18cf71614c4c655ead5317b99edbeeb32b65a8
Glenn Arnold Barosen <glennbarosen@gmail.com>
2022-01-17 19:09:54 +0100

    quickfix errors

--- src/components/LastMatchTile.tsx
+++ src/components/LastMatchTile.tsx
@@ -1,59 +1,50 @@
-import React, { useContext } from 'react'
+import React from "react";
 
 /** @jsxRuntime classic */
 /**@jsx jsx */
-import { jsx } from '@emotion/react'
-import { matchTime, stat, statNumber, tile } from '../styles/AppStyles'
-import { AppContext } from '../context/AppContext'
-import { format } from 'date-fns'
+import { jsx } from "@emotion/react";
+import { matchTime, stat, statNumber, tile } from "../styles/AppStyles";
 
 interface IPlayerTileProps {
-    player: any
+  player: any;
 }
-
 
 const LastMatchTile = (props: IPlayerTileProps) => {
+  const kills = props?.player?.matches[0]?.playerStats?.kills;
+  const KD = props?.player?.matches[0]?.playerStats?.kdRatio;
+  const damage = props?.player?.matches[0]?.playerStats?.damageDone;
+  const moving = `${Math.round(
+    props?.player?.matches[0]?.playerStats?.percentTimeMoving
+  )}%`;
+  const placement = props?.player?.matches[0]?.playerStats?.teamPlacement;
+  //   const utcSec = props?.player?.matches[0]?.utcEndSeconds;
 
-    const kills = props.player.matches[0].playerStats.kills
-    const KD = props.player.matches[0].playerStats.kdRatio
-    const damage = props.player.matches[0].playerStats.damageDone
-    const moving = `${Math.round(props.player.matches[0].playerStats.percentTimeMoving)}%`
-    const placement = props.player.matches[0].playerStats.teamPlacement
-    const utcSec = props.player.matches[0].utcEndSeconds
-    const matchEnd = format(new Date(utcSec * 1000), 'dd.MMM HH:mm')
+  return (
+    <div css={tile}>
+      <h1>{props?.player?.matches[0]?.player?.username}</h1>
+      <div css={stat}>
+        <p>Kills:</p>
+        <p css={statNumber}>{kills}</p>
+      </div>
+      <div css={stat}>
+        <p>KD:</p>
+        <p css={statNumber}>{KD}</p>
+      </div>
+      <div css={stat}>
+        <p>Damage:</p>
+        <p css={statNumber}>{damage}</p>
+      </div>
+      <div css={stat}>
+        <p>Time moving:</p>
+        <p css={statNumber}>{moving}</p>
+      </div>
+      <div css={stat}>
+        <p>Placement:</p>
+        <p css={statNumber}>{placement}</p>
+      </div>
+      <div css={matchTime}>{/* <p>Match ended: {matchEnd}</p> */}</div>
+    </div>
+  );
+};
 
-
-    return (
-        <div css={tile}>
-            <h1>{props.player.matches[0].player.username}</h1>
-            <div css={stat}>
-                <p>Kills:</p>
-                <p css={statNumber}>{kills}</p>
-            </div>
-            <div css={stat}>
-                <p>KD:</p>
-                <p css={statNumber}>{KD}</p>
-            </div>
-            <div css={stat}>
-                <p>Damage:</p>
-                <p css={statNumber}>{damage}</p>
-            </div>
-            <div css={stat}>
-                <p>Time moving:</p>
-                <p css={statNumber}>{moving}</p>
-            </div>
-            <div css={stat}>
-                <p>Placement:</p>
-                <p css={statNumber}>{placement}</p>
-            </div>
-            <div css={matchTime}>
-                <p>Match ended: {matchEnd}</p>
-            </div>
-
-        </div>
-
-
-    )
-}
-
-export default LastMatchTile
+export default LastMatchTile;
--- src/components/TileRow.tsx
+++ src/components/TileRow.tsx
@@ -1,30 +1,18 @@
-import React, { Fragment, useContext } from 'react'
-import { rowTitle, tileContainer } from '../styles/AppStyles'
+import React, { Fragment } from "react";
+import { rowTitle, tileContainer } from "../styles/AppStyles";
 /** @jsxRuntime classic */
 /**@jsx jsx */
-import { jsx } from '@emotion/react'
-import PlayerTile from './LastMatchTile'
-import { AppContext } from '../context/AppContext'
-
-
-
-
+import { jsx } from "@emotion/react";
 
 const TileRow = (props: any) => {
-    const appContext = useContext(AppContext)
+  return (
+    <Fragment>
+      <div css={rowTitle}>
+        <h3>{props.title}</h3>
+      </div>
+      <div css={tileContainer}>{props.children}</div>
+    </Fragment>
+  );
+};
 
-    return (
-        <Fragment>
-            <div css={rowTitle}>
-                <h3>{props.title}</h3>
-            </div>
-            <div css={tileContainer}>
-                {props.children}
-
-            </div>
-        </Fragment>
-    )
-
-}
-
-export default TileRow
+export default TileRow;
